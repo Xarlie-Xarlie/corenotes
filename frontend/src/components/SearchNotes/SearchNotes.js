0c094802b40a5415395743913a8f4b21a06bb455
charliecharlie <jeancharles552@gmail.com>
2024-08-16 17:30:34 -0300

    feat: search note mobile styling

--- frontend/src/components/SearchNotes/SearchNotes.js
+++ frontend/src/components/SearchNotes/SearchNotes.js
@@ -27,12 +27,12 @@ function SearchNotes({ onSearch }) {
   };
 
   return (
-    <header className="bg-white px-8 py-4">
-      <div className="flex justify-between">
-        <div className="flex gap-4 grow">
-          <img className="size-12" src={noteIcon} alt="NoteIcon" />
-          <h4 className="text-xl text-[#455A64] p-2">CoreNotes</h4>
-          <div className="flex ml-4  py-2 px-4 grow max-w-xl shadow-md border rounded-md border-[#D9D9D9]">
+    <header className="bg-white px-2 py-4 sm:px-8 sm:py-4">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-1 sm:gap-4 grow mx-4 items-center">
+          <img className="size-8 md:size-12" src={noteIcon} alt="NoteIcon" />
+          <h4 className="invisible h-0 w-0 sm:visible sm:h-fit sm:w-fit text-xs sm:text-xl text-[#455A64]">CoreNotes</h4>
+          <div className="flex items-center p-2 grow md:max-w-xl shadow-md border rounded-md border-[#D9D9D9]">
             <input
               className="grow focus:outline-none text-[#9A9A9A]"
               placeholder="Pesquisar notas"
@@ -41,7 +41,7 @@ function SearchNotes({ onSearch }) {
               onKeyDown={handleKeyDown}
             />
             <img
-              className="size-6 cursor-pointer"
+              className="size-4 sm:size-6 cursor-pointer"
               src={searchIcon}
               alt="SearchIcon"
               onClick={handleSearchClick}
@@ -50,7 +50,7 @@ function SearchNotes({ onSearch }) {
         </div>
         <div>
           <img
-            className="size-8 my-2 cursor-pointer"
+            className="size-6 md:size-8 cursor-pointer"
             src={cross}
             alt="CrossIcon"
             onClick={clearSearch}
