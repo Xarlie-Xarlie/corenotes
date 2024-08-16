e046f398201c1146da8fdbdcfc4f7c7a9dbade9d
charliecharlie <jeancharles552@gmail.com>
2024-08-16 00:48:52 -0300

    feat: added seed to notes

---
+++ backend/src/seeders/20240816030507-demo-notes.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const { faker } = require('@faker-js/faker');
+
+const color = [
+  '#FFFFFF',
+  '#BAE2FF',
+  '#B9FFDD',
+  '#FFE8AC',
+  '#FFCAB9',
+  '#F99494',
+  '#9DD6FF',
+  '#ECA1FF',
+  '#DAFF8B',
+  '#FFA285',
+  '#CDCDCD',
+  '#979797',
+  '#A99A7C'];
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    var notes = [];
+    var jobArea;
+    for (var i = 0; i < 50; i++) {
+      jobArea = faker.person.jobArea();
+      notes.push({
+        title: jobArea,
+        description: faker.lorem.lines(4),
+        favorite: faker.datatype.boolean(),
+        color: faker.helpers.arrayElement(color),
+        search: Sequelize.fn('to_tsvector', jobArea),
+        createdAt: faker.date.anytime(),
+        updatedAt: faker.date.anytime(),
+      });
+    }
+    await queryInterface.bulkInsert('Notes', notes, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Notes', null, {});
+  }
+};
